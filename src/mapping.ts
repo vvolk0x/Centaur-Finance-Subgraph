a9aba6c366e7258443133d5e9faac2e89851146d
vvolk0x <75569050+vvolk0x@users.noreply.github.com>
2020-12-06 20:24:47 +0500

    Add files via upload

---
+++ src/mapping.ts
@@ -0,0 +1,58 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import {
+  CentaurToken,
+  Approval,
+  OwnershipTransferred,
+  Paused,
+  Transfer,
+  Unpaused
+} from "../generated/CentaurToken/CentaurToken"
+import { _Approval, _OwnershipTransferred, _Transfer } from "../generated/schema"
+
+export function handleApproval(event: Approval): void {
+  let entity = _Approval.load(event.params.value.toHex())
+
+  if (entity == null) {
+    entity = new _Approval(event.params.value.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+  entity.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let entity = _OwnershipTransferred.load(event.params.newOwner.toHex())
+
+  if (entity == null) {
+    entity = new _OwnershipTransferred(event.params.newOwner.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+  entity.save()
+}
+
+export function handlePaused(event: Paused): void {}
+
+export function handleTransfer(event: Transfer): void {
+  let entity = _Transfer.load(event.params.value.toHex())
+
+  if (entity == null) {
+    entity = new _Transfer(event.params.value.toHex())
+    entity.count = BigInt.fromI32(0)
+  }
+
+  entity.count = entity.count + BigInt.fromI32(1)
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+  entity.save()
+}
+
+export function handleUnpaused(event: Unpaused): void {}
